3f783b34d290e66097a4160df14a7dcd5731530c
Vasiliy Zalatov <55500552+keyldev@users.noreply.github.com>
2022-02-01 19:29:17 +0300

    Add files via upload

--- stack/Generic.java
+++ stack/Generic.java
@@ -3,16 +3,17 @@ package stack;
 import java.util.*;
 
 public class Generic<T>{
+	//fix initialization
 	private Collection<T> defaultCollection = new Stack<T>();
 	public void addGroup(Stack<T> group) {
-		if(defaultCollection != null)
+		if(defaultCollection != null) // mozhno udalit
 		{
 			for(int i = 0; i < group.size(); i++)
 			{
-				this.defaultCollection.add(group.get(i));
+				this.defaultCollection.add(group.get(i)); // this is for adding to ready stack
 			}
 		}
-		else this.defaultCollection = group;
+		else this.defaultCollection = group; // if null 
 		System.out.println("debug-----\n" + defaultCollection.toString());
 	}
 
